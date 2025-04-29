bed71088fc659829d2d543783416d81d698bb07f
nauflabhy <n4uf4l.ku@gmail.com>
2025-04-29 18:40:10 +0700

    Membuat object mahasiswa

--- OOP.cpp
+++ OOP.cpp
@@ -6,11 +6,14 @@ public:
     int nim;
     string nama;
     float nilai;
-};
 
 void printData () {
-    
-}
+    cout << "NIM:" << nim << endl;
+    cout << "Nama:" << nama << endl;
+    cout << "Nilai:" << nilai << endl;
+    }
+
+};
 
 int main () {
 
